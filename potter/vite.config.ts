897107689d015a524176a78061c84c951896273b
Lisowez <lisowez@icloud.com>
2024-07-04 18:24:52 +0300

    fix:fix configs

--- potter/vite.config.ts
+++ potter/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,5 +12,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "src/setupTests",
     mockReset: true,
+  },
+  build: {
+    rollupOptions: {
+      external: ["vite.config.d.ts"],
+    },
   },
 })
