ee67870ddd1f390e9b0f256cf2596c5159190149
Lisowez <lisowez@icloud.com>
2024-07-17 20:46:09 +0300

    feat: add response transform

--- potter/src/App/store/api/api.tsx
+++ potter/src/App/store/api/api.tsx
@@ -1,15 +1,21 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { Character } from "../../../utils/interface/Character"
+import {
+  FetchBaseQueryMeta,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react"
+import { NewInterfaceForData, transformData } from "./transformAPI"
 
 export const api = createApi({
   reducerPath: "potterApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://hp-api.onrender.com/api" }),
   endpoints: builder => ({
-    getCharacters: builder.query<Character[], void>({
+    getCharacters: builder.query<NewInterfaceForData[], void>({
       query: () => "/characters",
+      transformResponse: transformData,
     }),
-    getCharacterByID: builder.query<Character[], string>({
+    getCharacterByID: builder.query<NewInterfaceForData[], string>({
       query: (id: string) => `/character/${id}`,
+      transformResponse: transformData,
     }),
   }),
 })
