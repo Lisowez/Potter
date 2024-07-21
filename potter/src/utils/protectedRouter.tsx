2b46b26e0443538aaa7c8097113f4fe2a8ec5cb4
Lisowez <lisowez@icloud.com>
2024-07-21 18:54:45 +0300

    fix: fix protected router

--- potter/src/utils/protectedRouter.tsx
+++ potter/src/utils/protectedRouter.tsx
@@ -1,21 +1,33 @@
 import { useDispatch, useSelector } from "react-redux"
-import { RootState } from "../App/store/store"
 import { Navigate } from "react-router-dom"
 import { getUserIsLoggedIn, loadUserData } from "../App/store/userSlice"
 import { getUserActive } from "./workUser/forWorkWithUser"
 import { useEffect, useState } from "react"
 import { Loading } from "../pages/Loading/Loading"
-
 type Props = {
   page: JSX.Element
 }
 
 export const ProtectedRoute = ({ page }: Props) => {
+  const [isLoading, setIsLoading] = useState(true)
   const isLoggedIn = useSelector(getUserIsLoggedIn)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const user = getUserActive()
+    if (user) {
+      const userData = JSON.parse(user)
+      dispatch(loadUserData({ user: userData }))
+      setIsLoading(false)
+    }
+  }, [dispatch])
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   if (!isLoggedIn) {
     return <Navigate to="/" />
   }
-
   return page
 }
